847e875ab98960e657b9646efc6d472719e585b3
Ross A. Baker <baker@alumni.indiana.edu>
2013-10-23 00:35:12 -0400

    Doesn't need implicit macro.

--- scalatra/src/main/scala/org/http4s/scalatra/package.scala
+++ scalatra/src/main/scala/org/http4s/scalatra/package.scala
@@ -11,11 +11,5 @@ package object scalatra {
   trait CanUnwrap[A]
 
   @compileTimeOnly("FAIL")
-  def canUnwrapImpl[A](a: A): CanUnwrap[A] = new CanUnwrap[A] {}
-
-  implicit def canUnwrap[A](implicit a: A): org.http4s.scalatra.CanUnwrap[A] =
-    macro canUnwrapMacroImpl[A]
-
-  def canUnwrapMacroImpl[A: c.WeakTypeTag](c: MContext)(a: c.Expr[A]): c.Expr[org.http4s.scalatra.CanUnwrap[A]] =
-    c.universe.reify { canUnwrapImpl[A](a.splice) }
+  implicit def canUnwrapImpl[A](implicit a: A): CanUnwrap[A] = new CanUnwrap[A] {}
 }
